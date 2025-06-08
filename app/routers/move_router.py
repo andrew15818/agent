2cd3788ec4ed1832849eedcad0c2f229a1f27df6
Andres Ponce <andreseeponce@gmail.com>
2025-06-08 23:15:23 +0800

    Fix: Add python chess import

--- app/routers/move_router.py
+++ app/routers/move_router.py
@@ -1,8 +1,14 @@
-from fastapi import APIRouter
+import dotenv
+import chess
+from fastapi import APIRouter, Request, Depends
+from app.services.move_service import get_next_move
 
 move_router = APIRouter(prefix="/move", tags=["moves"])
 
 
 @move_router.get("/move")
-async def get_next_move() -> None:
+async def get_next_move(request: Request):
+    print(request)
+    return {"message": "scholar's mate :D"}
+    # return get_next_move(request.headers["move"])
     pass
