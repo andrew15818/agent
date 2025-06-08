3f25d5dc0807274f1af7dfa0aef432f91e9af5ef
Andres Ponce <andreseeponce@gmail.com>
2025-06-08 23:15:40 +0800

    Fix: Settle on mistralAi implementation for testing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from google import genai
 from contextlib import asynccontextmanager
 from app.routers.move_router import move_router
+from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
 
 
@@ -11,10 +12,10 @@ async def lifespan(app: FastAPI):
     # Initialize permanent or shared resources
     load_dotenv()
 
-    client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-    app.state.client = client
+    app.state.llm = init_chat_model(os.getenv("MODEL_NAME"), model_provider="mistralai")
+    app.state.system_prompt = os.getenv("SYSTEM_PROMPT")
+    print(f"System prompt: {app.state.system_prompt}")
     yield
-
     # Close permanent or shared resources
 
 
