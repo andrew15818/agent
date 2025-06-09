1aa68bbd5aa932769bb6a4841e905425e1412b25
Andres Ponce <andreseeponce@gmail.com>
2025-06-09 22:39:25 +0800

    Fix: Improved docstring for tool registration

--- app/services/move_service.py
+++ app/services/move_service.py
@@ -1,3 +1,4 @@
+import chess
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.tools import tool
@@ -5,15 +6,18 @@ from google import genai
 
 
 @tool
-def get_next_move(history: list[str]) -> str:
+def calculate_next_move(history: list[str], current_move: str, color: str) -> str:
     """Get the next move from the LLM given the move history.
 
     Args:
         history (list[str]): The list of moves made so far.
+        move (str): The current move played.
+        color (str): The color of the current move.
     Returns:
         str: The next move in algebraic notation.
     """
-    pass
+
+    return "kf3"
 
 
 def check_if_move_is_valid() -> None:
