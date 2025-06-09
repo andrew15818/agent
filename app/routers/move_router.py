53d248d008cd83dcd06475e6c4543c36f41e3264
Andres Ponce <andreseeponce@gmail.com>
2025-06-09 22:39:00 +0800

    Dummy service layer call for testing

--- app/routers/move_router.py
+++ app/routers/move_router.py
@@ -1,14 +1,12 @@
-import dotenv
 import chess
-from fastapi import APIRouter, Request, Depends
-from app.services.move_service import get_next_move
+from fastapi import APIRouter, Request
+from app.services.move_service import calculate_next_move
 
 move_router = APIRouter(prefix="/move", tags=["moves"])
 
 
-@move_router.get("/move")
-async def get_next_move(request: Request):
-    print(request)
-    return {"message": "scholar's mate :D"}
-    # return get_next_move(request.headers["move"])
-    pass
+@move_router.get("/move/{move}")
+async def get_next_move(request: Request, move: str):
+    move_history = request.app.state.move_history
+    request.app.state.llm.invoke({"color": "white", "move_history": move})
+    return calculate_next_move()
