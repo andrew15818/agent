6cbc309f1cf50e2ec7130fed57ee073afdd87785
Andres Ponce <andreseeponce@gmail.com>
2025-06-10 22:33:32 +0800

    Fix: Separate LLM-code into separate class, todo manage state passing

--- app/services/move_service.py
+++ app/services/move_service.py
@@ -1,8 +1,41 @@
+import os
+
 import chess
+from dotenv import load_dotenv
+from google import genai
+from langchain.chat_models import init_chat_model
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.tools import tool
-from google import genai
+
+
+class LLMManager:
+    """
+    Group LLM-related methods together in a single class.
+    """
+
+    def __init__(self):
+        self.llm = init_chat_model(os.getenv("MODEL_NAME"), model_provider="mistralai")
+        self.system_prompt = os.getenv("SYSTEM_PROMPT")
+        self.move_history = []
+        self.prompt_template = ChatPromptTemplate([("system", self.system_prompt)])
+        self.prompt_template.invoke(
+            {"color": "white", "move_history": self.move_history}
+        )
+        self.chain = self.prompt_template | self.llm
+        print(type(self.chain))
+        print(f"System prompt: {self.system_prompt}")
+
+        pass
+
+
+class BoardManager:
+    """
+    Manage the game board via the chess library.
+    """
+
+    def __init__(self):
+        pass
 
 
 @tool
--- main.py
+++ main.py
@@ -3,11 +3,8 @@ from contextlib import asynccontextmanager
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from langchain.chat_models import init_chat_model
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.messages import HumanMessage
-
 from app.routers.move_router import move_router
+from app.services.move_service import LLMManager, BoardManager
 
 
 @asynccontextmanager
@@ -15,23 +12,8 @@ async def lifespan(app: FastAPI):
     # Initialize permanent or shared resources
     load_dotenv()
 
-    # TODO: Make the actual invocation
-    app.state.llm = init_chat_model(os.getenv("MODEL_NAME"), model_provider="mistralai")
-    app.state.system_prompt = os.getenv("SYSTEM_PROMPT")
-    app.state.move_history = []
-    app.state.prompt_template = ChatPromptTemplate(
-        [("system", app.state.system_prompt)]
-    )
-    app.state.prompt_template.invoke(
-        {"color": "white", "move_history": app.state.move_history}
-    )
-    app.state.chain = app.state.prompt_template | app.state.llm
-    print(type(app.state.chain))
-    print(f"System prompt: {app.state.system_prompt}")
-    result = app.state.chain.invoke(
-        {"color": "white", "move_history": app.state.move_history}
-    )
-    print(result)
+    app.state.llm_manager = LLMManager()
+    app.state.board_manager = BoardManager()
     yield
     # Close permanent or shared resources
 
