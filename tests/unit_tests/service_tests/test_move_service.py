dc804f4959c08f63d10281298cc1b6eb71072a7d
Andres Ponce <andreseeponce@gmail.com>
2025-06-16 22:44:19 +0800

    Feat: Initial unit, mock testing skeleton with pytest, unittest

---
+++ tests/unit_tests/service_tests/test_move_service.py
@@ -0,0 +1,37 @@
+from unittest.mock import MagicMock, Mock, patch
+
+import pytest
+from fastapi.testclient import TestClient
+
+from app.services.move_service import BoardManager, LLMManager, calculate_next_move
+
+
+class TestBoardManager:
+    pass
+
+
+class TestLLMManager:
+    """Test LLMManager initialization and next move generation"""
+
+    @patch.dict(
+        {
+            "MODEL_NAME": "mistral-small",
+            "SYSTEM_PROMPT": "You are a professional chess player playing as {color}. Given the move history, your task is to generate the best move given the context, and return it as a JSON object.",
+            "HUMAN_PROMPT": "Given the move history {move_history}, play the best legal move and return your answer as json: ",
+        }
+    )
+    @patch("app.services.move_service.init_chat_model")
+    def test_llm_manager_init(self, mock_init_chat_model):
+        """Ensure the LLMManager class initializes properly"""
+        mock_llm = Mock()
+        mock_init_chat_model.return_value = mock_llm
+
+        llm_manager = LLMManager()
+        pass
+
+
+def test_calculate_next_move():
+    """Test the move service's calcualation of the next move."""
+    scholars_mate_sequence = ["e4", "e5", "Qh5", "Nc6", "Bc4", "Nf6", "Qxf7"]
+    # TODO: Pass the move sequence to the LLM
+    pass
