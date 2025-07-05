aad5455f53e9d7710e6d85f13544d103de465533
Andres Ponce <andreseeponce@gmail.com>
2025-07-05 17:31:14 +0800

    Fix: Change get_next_move to POST

--- app/routers/move_router.py
+++ app/routers/move_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request, Depends
 from app.services.move_service import calculate_next_move
 from app.services.move_service import LLMManager
+from app.schemas.move_schemas import MoveSchema
 
 move_router = APIRouter(prefix="/move", tags=["moves"])
 
@@ -9,8 +10,8 @@ def get_llm_manager(request: Request) -> LLMManager:
     return request.app.state.llm_manager
 
 
-@move_router.get("/move/{move}")
-async def get_next_move(move: str, LLMManager=Depends(get_llm_manager)):
+@move_router.post("/move")
+async def get_next_move(move: MoveSchema, LLMManager=Depends(get_llm_manager)):
     return calculate_next_move(LLMManager, move)
 
 
