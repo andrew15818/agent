20a1b43924cade6df87bfaf6a382a9fa32404385
Andres Ponce <andreseeponce@gmail.com>
2025-07-05 17:31:29 +0800

    Feat: empty comment value for easier testing

--- app/schemas/move_schemas.py
+++ app/schemas/move_schemas.py
@@ -1,9 +1,8 @@
 from pydantic import BaseModel
-from fastapi import Request
 
 
 # Standard for communicating move between frontend/backend
 class MoveSchema(BaseModel):
     value: str
-    comment: str
-    game_status: str
+    comment: str = ""
+    game_status: str = "ongoing"
