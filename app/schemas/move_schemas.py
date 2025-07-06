846571f01af20ba966aff20b5c596d417b16b411
Andres Ponce <andreseeponce@gmail.com>
2025-07-06 22:37:42 +0800

    Fix: Add GameStatus as enum instead of str

--- app/schemas/move_schemas.py
+++ app/schemas/move_schemas.py
@@ -1,8 +1,15 @@
 from pydantic import BaseModel
+from enum import Enum
+
+
+class GameStatus(str, Enum):
+    not_started = "not_started"
+    ongoing = "ongoing"
+    finished = "finished"
 
 
 # Standard for communicating move between frontend/backend
 class MoveSchema(BaseModel):
     value: str
     comment: str = ""
-    game_status: str = "ongoing"
+    game_status: GameStatus = GameStatus.not_started
